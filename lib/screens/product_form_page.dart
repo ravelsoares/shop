30b002e8a4fa6af0d8e3679eb977e0334dcc7da5
ravelsoares <ravelsoares64@gmail.com>
2022-04-12 16:28:42 -0300

    Adicionando TextFormField

--- lib/screens/product_form_page.dart
+++ lib/screens/product_form_page.dart
@@ -1,18 +1,60 @@
 import 'package:flutter/material.dart';
 
 class ProductFormPage extends StatefulWidget {
-  const ProductFormPage({ Key? key }) : super(key: key);
+  const ProductFormPage({Key? key}) : super(key: key);
 
   @override
   State<ProductFormPage> createState() => _ProductFormPageState();
 }
 
 class _ProductFormPageState extends State<ProductFormPage> {
+  final _priceFocus = FocusNode();
+  final _descriptionFocus = FocusNode();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _priceFocus.dispose();
+    _descriptionFocus.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Formulário de produto'),
+        title: const Text('Formulário de produto'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Form(
+          child: ListView(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(labelText: 'Nome'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocus);
+                },
+              ),
+              TextFormField(
+                focusNode: _priceFocus,
+                decoration: const InputDecoration(labelText: 'Preço'),
+                textInputAction: TextInputAction.next,
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocus);
+                },
+              ),
+              TextFormField(
+                focusNode: _descriptionFocus,
+                decoration: const InputDecoration(labelText: 'Descrição'),
+                keyboardType: TextInputType.multiline,
+                maxLines: 3,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
